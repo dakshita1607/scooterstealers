6b605f1737e9078e65eddc903bc5f44a2089cdb5
dakshita1607 <dakshitagarg16@gmail.com>
2025-07-29 22:06:36 -0400

    Road Dashed Lines

--- scooters_stealers.py
+++ scooters_stealers.py
@@ -17,8 +17,40 @@ def redrawAll(app):
 def drawRoad(app): 
     drawRect(app.roadX, 0, app.roadWidth, app.height, fill = 'darkGrey') #Main Road
     
-    drawRect(app.roadX - 10, 0, 10, app.height, fill = 'grey')
-    drawRect(app.roadX + app.roadWidth, 0, 10, app.height, fill = 'grey')
-    
+    drawRect(app.roadX - 10, 0, 10, app.height, fill = 'grey') #Road Edge
+    drawRect(app.roadX + app.roadWidth, 0, 10, app.height, fill = 'grey') #Road Edge
 
+    laneWidth = app.roadWidth // 3
+
+    drawRect(app.roadX + laneWidth -2, 0, 4, app.height, fill = 'white') #Left Lane Divider
+    drawRect(app.roadX + 2 * laneWidth -2, 0, 4, app.height, fill = 'white' ) #Right Lane Divider
+    
+    drawMovingLaneLines(app)
+
+def drawMovingLaneLines(app):
+    laneWidth = app.roadWidth // 3
+
+    leftCenter = app.roadX + laneWidth //2
+    rightCenter = app.roadX + laneWidth + laneWidth // 2
+
+    dashLength = 30
+    dashGap = 20
+
+    y = app.roadLineOffset 
+
+    while y < app.height: 
+        if y + dashLength > 0:
+            drawRect(leftCenter - 2, y, 4, dashLength, fill = 'yellow')
+            drawRect(rightCenter - 2, y, 4, dashLength, fill = 'yellow')
+        y += dashLength + dashGap
+
+
+def onStep(app): 
+    app.roadLineOffset += 3
+
+    if app.roadLineOffset > 50: 
+        app.roadLineOffset = 0
 runApp()
+
+
+# No line in the right most row, needs fixing
