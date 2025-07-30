77a0eaef288a0c045632d66bf155cb44855c35a2
dakshita1607 <dakshitagarg16@gmail.com>
2025-07-30 01:05:47 -0400

    Coin Code Update

--- scooters_stealers.py
+++ scooters_stealers.py
@@ -1,4 +1,5 @@
 from cmu_graphics import *
+import random
 
 def onAppStart(app):
     app.width = 400
@@ -12,7 +13,7 @@ def onAppStart(app):
     app.roadLineOffset = 0
 
     #Player
-    app.playerImageUrl = 'https://tinyurl.com/4xemdrhk'
+    app.playerImageUrl = 'https://us-east.storage.cloudconvert.com/tasks/4b4ff701-4128-481e-92e3-c43673fe7467/image-removebg-preview%20%281%29%202.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=cloudconvert-production%2F20250730%2Fva%2Fs3%2Faws4_request&X-Amz-Date=20250730T044221Z&X-Amz-Expires=86400&X-Amz-Signature=5bb70c0ea557d8814bddac0c11927da6088e43063febc6c76c6f90e0375668cf&X-Amz-SignedHeaders=host&response-content-disposition=inline%3B%20filename%3D%22image-removebg-preview%20%281%29%202.png%22&response-content-type=image%2Fpng&x-id=GetObject'
     app.playerSize = 70
     app.playerY = app.height - 100
     app.playerLane = 1
@@ -111,5 +112,50 @@ def onStep(app):
     if app.roadLineOffset > 50: 
         app.roadLineOffset = 0
 
-    #AHH THERES SO MUCH TO WRITE OMGGGG 
+    #Coin Spawn
+    if app.coinSpawnTimer >= app.coinSpawndx: 
+        spawnCoin(app)
+        app.coinSpawnTimer = 0
+
+    updateCoins(app)
+    checkCoinCollision(app)
+
+
+def drawScore(app): 
+    drawLabel(f'Score: {app.score}', 50, 30, fill='black', size = 20, bold = True)
+
+def spawnCoin(app): 
+    lane = random.randint(0,2) #Choose a lane for coin randomly
+    coin = {
+        'x': app.lanePositions[lane],
+        'y': -app.coinSize, #Start above screen
+        'lane': lane
+    }
+    app.coins.append(coin)
+
+def updateCoins(app): 
+    for coin in app.coins: 
+        coin['y'] += app.coinSpeed #Move coins down the screen
+    
+    app.coins = [coin for coin in app.coins if coin['y'] < app.height + app.coinSize]
+
+def checkCoinCollision(app): 
+    playerX = app.lanePositions[app.playerLane]
+    playerY = app.playerY 
+
+    coinsToRemove = []
+
+    i = 0 
+    while i < len(app.coins): 
+        coin = app.coins[i]
+        if coin['lane'] == app.playerLane:
+            distance = ((playerX-coin['x'])**2 + (playerY-coin['y']**2)**0.5)
+            if distance < (app.playerSize + app.coinSize) // 2: 
+                app.coins.pop(i)
+                app.score += 10
+            else:
+                i += 1
+        else:
+            i += 1
+
 runApp()
