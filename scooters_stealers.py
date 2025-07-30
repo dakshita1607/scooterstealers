3d1968738b9c4cc4226eba03a5074c3c927d2665
dakshita1607 <dakshitagarg16@gmail.com>
2025-07-29 21:41:20 -0400

    Road Code

--- scooters_stealers.py
+++ scooters_stealers.py
@@ -1,3 +1,24 @@
 from cmu_graphics import *
 
+def onAppStart(app):
+    app.width = 400
+    app.height = 600
+
+    #Road Properties 
+    app.roadWidth = 300 
+    app.roadX = (app.width- app.roadWidth) //2
+
+    #Animation for road lines
+    app.roadLineOffset = 0
+
+def redrawAll(app): 
+    drawRoad(app)
+
+def drawRoad(app): 
+    drawRect(app.roadX, 0, app.roadWidth, app.height, fill = 'darkGrey') #Main Road
+    
+    drawRect(app.roadX - 10, 0, 10, app.height, fill = 'grey')
+    drawRect(app.roadX + app.roadWidth, 0, 10, app.height, fill = 'grey')
+    
+
 runApp()
